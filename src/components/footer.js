979d4024063a690c3ffee49a9643bfc74eac5027
matthieu-brillaxis <matthieu.brillaxis@live.com>
2018-05-29 12:44:07 +0200

    add scroll to top

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import * as Scroll from 'react-scroll';
+import { animateScroll } from 'react-scroll';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faAngleUp from '@fortawesome/fontawesome-free-solid/faAngleUp';
 
+function handleClick() {
+  Scroll.animateScroll.scrollToTop();
+}
+
 const Footer = () => (
   <div className="footer">
-    <FontAwesomeIcon className="footer-round-button" onClick={Scroll.animateScroll.scrollToTop()} icon={faAngleUp} size="2x" />
+    <FontAwesomeIcon className="footer-round-button" icon={faAngleUp} onClick={handleClick} size="2x" />
     <p>
         mbrillaxis ©.
     </p>
